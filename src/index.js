a8e54f99a8e9766d94f9292aea80a709449e53a0
Mayara Maia <39660246+mayarovski@users.noreply.github.com>
2018-11-15 10:42:07 -0200

    Upload

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import registerServiceWorker from "./registerServiceWorker";
-import { render } from "react-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
